1610089b2fbf1620a672e5a4ed1b06e087a951ec
Alexandre Beiruth <101944430+Alebeiruth@users.noreply.github.com>
2024-03-18 18:50:49 -0300

    Update entregar_blc5.r

--- entregar_blc5.r
+++ entregar_blc5.r
@@ -4,6 +4,7 @@ library(VennDiagram)
 #Atividade da Semana 5
 
 #criacao das populacoes
+
 populTotal <- 50
 populA <- 15
 populB <- 10
@@ -12,6 +13,20 @@ populAB <- 6
 populAC <- 4
 populBC <- 3
 populABC <- 1
+
+#dataframe
+
+dfPopulacao <- expand.grid( populacaoTotal = populTotal, populacaoA = populA, 
+                            populacaoB = populB, populacaoC = populC,
+                            populacaoAB = populAB, populacaoAC = populAC,
+                            populacaoBC = populBC, populacaoABC = populABC)
+dfPopulacao
+
+
+pop <- paste(dfPopulacao$populacaoTotal, dfPopulacao$populacaoA, dfPopulacao$populacaoB,
+             dfPopulacao$populacaoC, dfPopulacao$populacaoAB, dfPopulacao$populacaoAC,
+             dfPopulacao$populacaoBC, dfPopulacao$populacaoABC)
+pop
 
 #se uma pessoa for entrevistada
 #qual a probabilidade dela utilizar somente um servico?
@@ -27,15 +42,12 @@ serviUsados <- populA + populB + populC - populAB - populAC - populBC + populABC
 menosUmServico <- serviUsados / populTotal
 menosUmServico
 
-#dataframe
-
 
 #diamagrama venn
 
-draw.triple.venn(popA = 15, popB = 10, popC = 8,
-                 sAB = 6, sAC = 4, sBC = 3, sABC = 1,
-                 category = c("A", "B", "C"), lty="blank",
-                 fill = c("skyblue", "red", "mediumorchid"))
-
-
+draw.triple.venn(areaA = 15, areaB = 10, areaC = 8,
+                 n12 = 6, n23 = 3, n13 = 4, # areaAB, areaBC, areaAC, respectivamente
+                 n123 = 1, # área de interseção entre A, B e C
+                 category = c("A", "B", "C"), # Categorias para os conjuntos A, B e C
+                 fill = c("skyblue", "red", "green")) # Cores para A, B e C
 
